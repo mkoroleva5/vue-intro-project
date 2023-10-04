7164b5311594e310dd5fb8110c2c1d9e92480807
mkoroleva5 <mkoroleva5@gmail.com>
2023-10-04 18:12:16 +0300

    feat: create Book modal

---
+++ src/components/BookModal.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import type { Book } from './BooksWidget.vue'
+
+defineProps<{
+  book: Book | null
+  onClose: () => void
+}>()
+</script>
+
+<template>
+  <div class="backdrop" @click="onClose()">
+    <div class="modal" @click="(e) => e.stopPropagation()">
+      <h3>{{ book?.title }}</h3>
+      <p>{{ book?.author }}</p>
+      <img :src="book?.img" :alt="book?.title" class="img" draggable="false" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.backdrop {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal {
+  width: 50%;
+  height: 80%;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.img {
+  margin: 10px;
+  object-fit: cover;
+  height: 85%;
+}
+</style>
